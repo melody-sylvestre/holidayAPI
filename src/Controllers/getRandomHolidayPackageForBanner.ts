be734c977c5c855384ff06215a77ca63a1c1300f
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-09-04 20:33:03 +0100

    still working on getRandomHolidayPackageForBanner route

--- src/Controllers/getRandomHolidayPackageForBanner.ts
+++ src/Controllers/getRandomHolidayPackageForBanner.ts
@@ -2,14 +2,35 @@ import {Request, Response} from 'express'
 import { bannerItem } from '../interfaces'
 import { UNSPLASH_ACCESS_KEY } from '../accessKey'
 import { getHolidayPackagesCollection } from '../db'
+import { validateHolidayPackage } from '../Validators/validateHolidayPackage'
 
 const getRandomHolidayPackageForBanner = async (request:Request, response:Response) => {
     const holidayPackagesCollection = await getHolidayPackagesCollection()
-    const holidayPackageDataFromDB = await holidayPackagesCollection.aggregate([ { $sample: { size: 1 } } ]).toArray()
-    const randomHolidayPackage = holidayPackageDataFromDB[0]
+    const allHolidayPackages = await holidayPackagesCollection.find({}).toArray()
+    
+    let holidayPackageIsValid :boolean = false 
+    const maxNumberOfAttempts :number = 10
 
-    // validate randomHolidayPackage -> should have a BannerImageUnsplashID + some other stuff
- 
+    let index:number = 0
+    let randomHolidayPackage = null
+
+    for (let i = 1; i <= maxNumberOfAttempts; i++ ) {
+        index = Math.floor(Math.random() * allHolidayPackages.length)
+        randomHolidayPackage = allHolidayPackages[index]
+
+        if (validateHolidayPackage(randomHolidayPackage)) {
+            break
+        } else if (i === maxNumberOfAttempts) {
+            const APIResponse = {
+                message: "Could not find a valid object in the database.",
+                data: {}
+            }
+            
+            response.status(400).json(APIResponse)
+            return 
+        }
+    }    
+    
    // should be in a try catch in order to catch potential issues with API
     const unsplashData = await fetch(`https://api.unsplash.com/photos/${randomHolidayPackage.BannerImageUnsplashID}`, {
         method: 'GET',
